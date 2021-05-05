bd85bb910e8f4c6707767e8f32753c6ac98c5330
wendyblt <wendy.billet@gmail.com>
2021-05-05 21:26:30 +0200

    [WEN] Init api-platform and create entity Address

---
+++ src/Entity/Address.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+#[ApiResource]
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     *
+     * @var string
+     */
+    private $postalCode;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string $postalCode
+     *
+     * @return Address
+     */
+    public function setPostalCode(string $postalCode): Address
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+}
