3ccf413f365cd12f2c8b506673e85f120a35179a
wendyblt <wendy.billet@gmail.com>
2021-05-07 09:13:38 +0200

    [WEN] Add entity Category and State

---
+++ src/Entity/Category.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+#[ApiResource]
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     *
+     * @var string
+     */
+    private $categoryName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategoryName(): string
+    {
+        return $this->categoryName;
+    }
+
+    /**
+     * @param string $categoryName
+     * @return Category
+     */
+    public function setCategoryName(string $categoryName): Category
+    {
+        $this->categoryName = $categoryName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Category
+     */
+    public function setDescription(string $description): Category
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+}
---
+++ src/Entity/State.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+#[ApiResource]
+class State
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     *
+     * @var string
+     */
+    private $stateName;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStateName(): string
+    {
+        return $this->stateName;
+    }
+
+    /**
+     * @param string $stateName
+     * @return State
+     */
+    public function setStateName(string $stateName): State
+    {
+        $this->stateName = $stateName;
+        return $this;
+    }
+}
